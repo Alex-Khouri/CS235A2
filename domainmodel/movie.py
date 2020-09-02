48dfe704af7022494189c6a53a4f3da1da54811c
alex <alex.khouri@gmail.com>
2020-09-02 13:41:35 +1200

    UPDATE

--- domainmodel/movie.py
+++ domainmodel/movie.py
@@ -1,32 +1,36 @@
 from domainmodel.genre import Genre
 from domainmodel.actor import Actor
 from domainmodel.director import Director
+from domainmodel.review import Review
 
 class Movie:
 	def __init__(self, titleArg=None, yearArg=None):
 		self.movie_title = None
-		if (isinstance(titleArg, str) and len(titleArg) > 0):
+		if isinstance(titleArg, str) and len(titleArg) > 0:
 			self.movie_title = titleArg.strip()
 		self.movie_year = None
-		if (isinstance(yearArg, int) and yearArg >= 1900):
+		if isinstance(yearArg, int) and yearArg >= 1900:
 			self.movie_year = yearArg
 		self.movie_description = None
 		self.movie_director = None
 		self.movie_actors = []
 		self.movie_genres = []
 		self.movie_runtime_minutes = None
+		self.movie_reviews = []
+		self.movie_rating = None
+		self.movie_votes = 0
 	
 	def __repr__(self):
 		return f"<Movie {self.movie_title}, {self.movie_year}>"
 	
 	def __eq__(self, other):
-		return (self.__class__ == other.__class__ and self.movie_title == other.movie_title and self.movie_year == other.movie_year)
+		return self.__class__ == other.__class__ and self.movie_title == other.movie_title and self.movie_year == other.movie_year
 	
 	def __lt__(self, other):
 		if self.movie_title == other.movie_title:
-			return (self.movie_year < other.movie_year)
+			return self.movie_year < other.movie_year
 		else:
-			return (self.movie_title < other.movie_title)
+			return self.movie_title < other.movie_title
 	
 	def __hash__(self):
 		return hash(self.movie_title + str(self.movie_year))
@@ -54,62 +58,103 @@ class Movie:
 	@property
 	def runtime_minutes(self):
 		return self.movie_runtime_minutes
-		
+
+	@property
+	def reviews(self):
+		return self.movie_reviews
+
+	@property
+	def rating(self):
+		return self.movie_rating
+
+	@property
+	def votes(self):
+		return self.movie_votes
+
 	@title.setter
 	def title(self, newTitle):
-		if (isinstance(newTitle, str) and len(newTitle) > 0):
+		if isinstance(newTitle, str) and len(newTitle) > 0:
 			self.movie_title = newTitle.strip()
 	
 	@description.setter
 	def description(self, newDescrip):
-		if (isinstance(newDescrip, str) and len(newDescrip) > 0):
+		if isinstance(newDescrip, str) and len(newDescrip) > 0:
 			self.movie_description = newDescrip.strip()
 	
 	@director.setter
 	def director(self, newDirector):
-		if (isinstance(newDirector, Director)):
+		if isinstance(newDirector, Director):
 			self.movie_director = newDirector
 			
 	@actors.setter
 	def actors(self, newActors):
-		if (isinstance(newActors, list)):
+		if isinstance(newActors, list):
 			self.movie_actors = newActors
 			
 	@genres.setter
 	def genres(self, newGenres):
-		if (isinstance(newGenres, list)):
+		if isinstance(newGenres, list):
 			self.movie_genres = newGenres
 			
 	@runtime_minutes.setter
 	def runtime_minutes(self, newRuntime):
-		if (isinstance(newRuntime, int)):
-			if (newRuntime >= 0):
+		if isinstance(newRuntime, int):
+			if newRuntime >= 0:
 				self.movie_runtime_minutes = newRuntime
 			else:
 				raise ValueError('ValueError: Negative runtime value!')
-			
+
+	@reviews.setter
+	def reviews(self, newReviews):
+		if isinstance(newReviews, list):
+			self.movie_reviews = newReviews
+
+	@rating.setter
+	def rating(self, newRating):
+		if isinstance(newRating, float):
+			self.movie_rating = newRating
+
+	@votes.setter
+	def votes(self, newVotes):
+		if isinstance(newVotes, int):
+			self.movie_votes = newVotes
+
 	def add_actor(self, newActor):
-		if (isinstance(newActor, Actor) and not newActor in self.movie_actors):
+		if isinstance(newActor, Actor) and not newActor in self.movie_actors:
 			self.movie_actors.append(newActor)
 			
 	def add_genre(self, newGenre):
-		if (isinstance(newGenre, Genre) and not newGenre in self.movie_genres):
+		if isinstance(newGenre, Genre) and not newGenre in self.movie_genres:
 			self.movie_genres.append(newGenre)
+
+	def add_review(self, newReview):
+		if isinstance(newReview, Review) and not newReview in self.movie_reviews:
+			self.movie_reviews.append(newReview)
+			v = self.movie_votes + 1
+			self.movie_rating = self.movie_rating*((v-1)/v) + newReview.rating*(1/v)
+			self.movie_votes = v
 			
 	def remove_actor(self, remActor):
-		if (isinstance(remActor, Actor) and remActor in self.movie_actors):
+		if isinstance(remActor, Actor) and remActor in self.movie_actors:
 			self.movie_actors.remove(remActor)
-		elif (isinstance(remActor, str)):
+		elif isinstance(remActor, str):
 			for actor in self.movie_actors:
 				if actor.actor_full_name == remActor:
 					self.movie_actors.remove(actor)
 					break
 			
 	def remove_genre(self, remGenre):
-		if (isinstance(remGenre, Genre) and remGenre in self.movie_genres):
+		if isinstance(remGenre, Genre) and remGenre in self.movie_genres:
 			self.movie_genres.remove(remGenre)
-		elif (isinstance(remGenre, str)):
+		elif isinstance(remGenre, str):
 			for genre in self.movie_genres:
 				if genre.genre_name == remGenre:
 					self.movie_genres.remove(genre)
 					break
+
+	def remove_review(self, remReview):
+		if isinstance((remReview, Review)) and remReview in self.movie_reviews:
+			self.movie_reviews.remove(remReview)
+			v = self.movie_votes - 1
+			self.movie_rating = self.movie_rating*((v+1)/v) - remReview.rating*(1/v)
+			self.movie_votes = v
--- domainmodel/review.py
+++ domainmodel/review.py
@@ -1,52 +1,63 @@
 from datetime import datetime
 
 from domainmodel.movie import Movie
+from domainmodel.user import User
 
 class Review:
-	def __init__(self, revMovie, revText, revRating):
-		self.revMovie = revMovie if isinstance(revMovie, Movie) else None
-		self.revText = revText.strip() if isinstance(revText, str) else None
-		self.revRating = revRating if (isinstance(revRating, int) and (revRating in range (1,11))) else None
-		self.revTimestamp = datetime.datetime.now()
+	def __init__(self, revUser, revMovie, revText, revRating):
+		self.review_user = revUser if isinstance(revUser, User) else None
+		self.review_movie = revMovie if isinstance(revMovie, Movie) else None
+		self.review_text = revText.strip() if isinstance(revText, str) else None
+		self.review_rating = revRating if (isinstance(revRating, int) and (revRating in range (1,11))) else None
+		self.review_timestamp = datetime.datetime.now()
 
 	def __repr__(self):
-		return f"<Review {self.revMovie}, {self.revRating}, {self.revTimestamp}, '{self.revText}'>"
+		return f"<Review {self.review_movie}, {self.review_rating}, {self.review_timestamp}, '{self.review_text}'>"
 
 	def __eq__(self, other):
-		return (self.revMovie == other.revMovie and self.revText == other.revText and self.revRating == other.revRating and self.revTimestamp == other.revTimestamp)
+		return self.review_movie == other.revMovie and self.review_text == other.revText and self.review_rating == other.revRating and self.review_timestamp == other.revTimestamp
 
 	@property
 	def movie(self):
-		return self.revMovie
+		return self.review_movie
 
 	@property
 	def review_text(self):
-		return self.revText
+		return self.review_text
 
 	@property
 	def rating(self):
-		return self.revRating
+		return self.review_rating
 
 	@property
 	def timestamp(self):
-		return self.revTimestamp
+		return self.review_timestamp
+
+	@property
+	def user(self):
+		return self.review_user
 
 	@movie.setter
 	def movie(self, newMovie):
-		if (isinstance(newMovie, Movie)):
-			self.revMovie = newMovie
+		if isinstance(newMovie, Movie):
+			self.review_movie = newMovie
 	
 	@review_text.setter
 	def review_text(self, newText):
-		if (isinstance(newText, str)):
-			self.revText = newText.strip()
+		if isinstance(newText, str):
+			self.review_text = newText.strip()
 
 	@rating.setter
 	def rating(self, newRating):
-		if (isinstance(newRating, int) and newRating in range(1,11)):
-			self.revText = newRating
+		if isinstance(newRating, int) and newRating in range(1, 11):
+			self.review_text = newRating
 
 	@timestamp.setter
 	def timestamp(self, newTimestamp):
-		if (isinstance(newTimestamp, datetime.datetime)):
-			self.revTimestamp = newTimestamp
+		if isinstance(newTimestamp, datetime.datetime):
+			self.review_timestamp = newTimestamp
+
+	@user.setter
+	def user(self, newUser):
+		if isinstance(newUser, User):
+			self.review_user = newUser
