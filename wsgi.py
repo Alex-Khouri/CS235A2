e061bf7f13f02d06b3c5982fdb67e895261ce079
alex <alex.khouri@gmail.com>
2020-09-06 23:00:18 +1200

    UPDATE

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-from repository import *
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
 
 if __name__ == "__main__":
-	app.run(host='localhost', port=8080)
+	app.run(host='localhost', port=5000, debug=True)
