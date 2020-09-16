71286ae20b23fadb2f8ac6c646e51e7d11175f0b
alex <alex.khouri@gmail.com>
2020-09-16 13:17:43 +1200

    UPDATE

--- wsgi.py
+++ wsgi.py
@@ -4,19 +4,23 @@ from domainmodel.user import User
 
 app = Flask(__name__)
 repo = MemoryRepo('datafiles/Data1000Movies.csv')
-serverData = {
+servData = {
+	"titleChars": ["0-9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
 	"allMovies": repo.movies,
 	"allActors": repo.actors,
 	"allDirectors": repo.directors,
 	"allGenres": repo.genres,
 	"allUsers": repo.users,
-	"currentUser": None
+	"currentUser": None,
+	"authMessage": "",
+	"filterQuery": "",
+	"filterCategory": ""
 }
 
 
 @app.route('/')
 def index():
-	return render_template('index.html', vars=serverData)
+	return render_template('index.html', vars=servData)
 
 @app.route('/login')
 def login():
@@ -24,10 +28,10 @@ def login():
 	password = request.args.get('LoginPassword')
 	user = repo.get_user(username)
 	if user is not None and password == user.password:
-		currentUser = user
-		return render_template('logged_in.html', vars=serverData)
+		servData["currentUser"] = user
+		return render_template('logged_in.html', vars=servData)
 	else:  # USE TEMPLATE TO DISPLAY FAILED LOGIN MESSAGE
-		return render_template('index.html', args=serverData)
+		return render_template('index.html', args=servData)
 
 @app.route('/register')
 def register():
@@ -35,28 +39,28 @@ def register():
 	password1 = request.args.get('RegPassword1')
 	password2 = request.args.get('RegPassword2')
 	if password1 == password2:
-		currentUser = User(username, password1)
-		repo.add_user(currentUser)
-		return render_template('logged_in.html', vars=serverData)
+		servData["currentUser"] = User(username, password1)
+		repo.add_user(servData["currentUser"])
+		return render_template('logged_in.html', vars=servData)
 	else:  # USE TEMPLATE TO DISPLAY FAILED REGISTRATION MESSAGE
-		return render_template('index.html', vars=serverData)
+		return render_template('index.html', vars=servData)
 
 @app.route('/logout')
 def logout():
-	currentUser = None
-	return render_template('index.html', vars=serverData)
+	servData["currentUser"] = None
+	return render_template('index.html', vars=servData)
 
 @app.route('/browse')
 def browse():
-	query = request.args.get("BrowseQuery")
-	category = request.args.get("BrowseCategory")
-	return render_template('index.html', vars=serverData)
+	servData["filterQuery"] = request.args.get("BrowseQuery")
+	servData["filterCategory"] = request.args.get("BrowseCategory")
+	return render_template('index.html', vars=servData)
 
 @app.route('/search')
 def search():
-	query = request.args.get("SearchQuery")
-	category = request.args.get("SearchCategory")
-	return render_template('index.html', vars=serverData)
+	servData["filterQuery"] = request.args.get("SearchQuery")
+	servData["filterCategory"] = request.args.get("SearchCategory")
+	return render_template('index.html', vars=servData)
 
 
 if __name__ == "__main__":
