48ab913571bd4ba1e9d3080d18ac43084d26e030
alex <alex.khouri@gmail.com>
2020-10-09 09:02:07 +1300

    Updated tests

--- tests/test_domain_model.py
+++ tests/test_domain_model.py
@@ -8,6 +8,18 @@ from getflix.domainmodel.review import Review
 from getflix.domainmodel.user import User
 from getflix.domainmodel.watchlist import Watchlist
 
+@pytest.fixture
+def user():
+    return User("Bob", "Password1")
+
+@pytest.fixture
+def watchlist():
+	watchlist = Watchlist()
+	watchlist.add_movie(Movie("Moana", 2016))
+	watchlist.add_movie(Movie("Ice Age", 2002))
+	watchlist.add_movie(Movie("Guardians of the Galaxy", 2012))
+	return watchlist
+
 
 # Actor tests
 def test_actor_add_actor_colleague():
@@ -18,12 +30,12 @@ def test_actor_add_actor_colleague():
 	assert len(actor2.colleagues) == 1
 
 def test_actor_remove_movie():
-	actor1 = Actor("John Doe")
+	actor = Actor("John Doe")
 	movie = Movie("Imception", 2011)
-	actor1.add_movie(movie)
-	assert len(actor1.movies) == 1
-	actor1.remove_movie(movie)
-	assert len(actor1.movies) == 0
+	actor.add_movie(movie)
+	assert len(actor.movies) == 1
+	actor.remove_movie(movie)
+	assert len(actor.movies) == 0
 
 def test_actor_check_if_this_actor_worked_with():
 	actor1 = Actor("John Doe")
@@ -71,25 +83,23 @@ def test_movie_remove_genre():
 
 
 # Review tests
-def test_review_init():
-	review = Review(User("Bob", "Password1"), Movie("Imception", 2011), "It was pretty weird", 6)
+def test_review_init(user):
+	review = Review(user, Movie("Imception", 2011), "It was pretty weird", 6)
 	assert str(review.user) == "<User bob>"
 	assert str(review.movie) == "<Movie Imception, 2011>"
 	assert str(review.text) == "It was pretty weird"
 
 
 # User tests
-def test_user_watch_movie():
-	user = User("Bob", "Password1")
+def test_user_watch_movie(user):
 	movie = Movie("Imception", 2011)
 	movie.runtime_minutes = 120
 	user.watch_movie(movie)
 	assert movie in user.watched_movies
 	assert user.time_spent_watching_movies_minutes == movie.runtime_minutes
 
-def test_user_remove_review():
-	user = User("Bob", "Password1")
-	review = Review(User("Bob", "Password1"), Movie("Imception", 2011), "It was pretty weird", 6)
+def test_user_remove_review(user):
+	review = Review(user, Movie("Imception", 2011), "It was pretty weird", 6)
 	user.add_review(review)
 	assert len(user.reviews) == 1
 	user.remove_review(review)
@@ -97,11 +107,7 @@ def test_user_remove_review():
 
 
 # Watchlist tests
-def test_watchlist_remove_movie():
-	watchlist = Watchlist()
-	watchlist.add_movie(Movie("Moana", 2016))
-	watchlist.add_movie(Movie("Ice Age", 2002))
-	watchlist.add_movie(Movie("Guardians of the Galaxy", 2012))
+def test_watchlist_remove_movie(watchlist):
 	watchlist.remove_movie(Movie("Moana", 2016))
 	watchlist.remove_movie(Movie("Guardians of the Galaxy", 2012))
 	test_output = ""
@@ -110,33 +116,24 @@ def test_watchlist_remove_movie():
 	assert test_output.strip() == "<Movie Ice Age, 2002>"
 	assert str(watchlist) == "[<Movie Ice Age, 2002>]"
 
-def test_watchlist_select_movie_to_watch():
-	watchlist = Watchlist()
-	watchlist.add_movie(Movie("Moana", 2016))
-	watchlist.add_movie(Movie("Ice Age", 2002))
-	watchlist.add_movie(Movie("Guardians of the Galaxy", 2012))
+def test_watchlist_select_movie_to_watch(watchlist):
 	assert str(watchlist.select_movie_to_watch(0)) == "<Movie Moana, 2016>"
 	assert str(watchlist.select_movie_to_watch(1)) == "<Movie Ice Age, 2002>"
 	assert str(watchlist.select_movie_to_watch(2)) == "<Movie Guardians of the Galaxy, 2012>"
 	assert str(watchlist.select_movie_to_watch(3)) == "None"
 	assert str(watchlist.select_movie_to_watch(-1)) == "None"
 
-def test_watchlist_size():
-	watchlist = Watchlist()
-	assert watchlist.size() == 0
-	watchlist.add_movie(Movie("Moana", 2016))
-	watchlist.add_movie(Movie("Ice Age", 2002))
-	watchlist.add_movie(Movie("Guardians of the Galaxy", 2012))
+def test_watchlist_size(watchlist):
 	assert watchlist.size() == 3
-	watchlist.remove_movie(Movie("Moana", 2016))
-	watchlist.remove_movie(Movie("Guardians of the Galaxy", 2012))
-	assert watchlist.size() == 1
+	watchlist.add_movie(Movie("Fight Club", 1999))
+	watchlist.add_movie(Movie("Se7en", 1995))
+	watchlist.add_movie(Movie("Memento", 2001))
+	assert watchlist.size() == 6
+	watchlist.remove_movie(Movie("Fight Club", 1999))
+	watchlist.remove_movie(Movie("Se7en", 1995))
+	assert watchlist.size() == 4
 
-def test_watchlist_first_movie_in_watchlist():
-	watchlist = Watchlist()
-	watchlist.add_movie(Movie("Moana", 2016))
-	watchlist.add_movie(Movie("Ice Age", 2002))
-	watchlist.add_movie(Movie("Guardians of the Galaxy", 2012))
+def test_watchlist_first_movie_in_watchlist(watchlist):
 	assert str(watchlist.first_movie_in_watchlist()) == "<Movie Moana, 2016>"
 	watchlist.remove_movie(Movie("Moana", 2016))
 	assert str(watchlist.first_movie_in_watchlist()) == "<Movie Ice Age, 2002>"
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -9,34 +9,35 @@ from getflix.domainmodel.review import Review
 from getflix.domainmodel.user import User
 from getflix.domainmodel.watchlist import Watchlist
 
-def test_repo_init():
-    repo = MemoryRepo('getflix/datafiles/Data1000Movies.csv')
+@pytest.fixture
+def repo():
+    return MemoryRepo('getflix/datafiles/Data1000Movies.csv')
+
+@pytest.fixture
+def user():
+    return User("Bob", "Password1")
+
+
+def test_repo_init(repo):
     assert len(repo.movies) == 1000
     assert len(repo.directors) == 644
     assert len(repo.actors) == 1985
     assert len(repo.genres) == 20
 
-def test_repo_remove_user():
-    repo = MemoryRepo('getflix/datafiles/Data1000Movies.csv')
-    user = User("Bob", "Password1")
+def test_repo_remove_user(repo, user):
     repo.add_user(user)
     repo.add_user(user)
     assert len(repo.users) == 1
     repo.remove_user(user)
     assert len(repo.users) == 0
 
-def test_repo_get_user():
-    repo = MemoryRepo('getflix/datafiles/Data1000Movies.csv')
-    user = User("Bob", "Password1")
+def test_repo_get_user(repo, user):
     repo.add_user(user)
     assert repo.get_user("bob") is user
 
-def test_repo_get_movie():
-    repo = MemoryRepo('getflix/datafiles/Data1000Movies.csv')
+def test_repo_get_movie(repo):
     assert str(repo.get_movie(" inception ")) == "<Movie Inception, 2010>"
 
-def test_repo_get_watchlist():
-    repo = MemoryRepo('getflix/datafiles/Data1000Movies.csv')
-    user = User("Bob", "Password1")
+def test_repo_get_watchlist(repo, user):
     repo.add_user(user)
     assert repo.get_watchlist("bob") is user.watchlist
